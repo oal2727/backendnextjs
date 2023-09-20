d3045c9337093bf198cb43d36c12e3abb0e3e36a
jose blas <joseluis_1555@hotmail.com>
2023-09-19 19:38:55 -0500

    corregir types

--- src/pages/api/app/contact.tsx
+++ src/pages/api/app/contact.tsx
@@ -3,30 +3,28 @@ import { prisma } from "utils/prisma"
 import { contactSchema } from "schemas/contactSchema"
 import cors from "../../../server/middlewares/cors"
 
-const handler = async(req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	await cors({ req, res, methods: [ "POST" ] })
-
-	if(req.method =="POST"){
+	if (req.method == "POST") {
 		const creds = await contactSchema.safeParseAsync(req.body)
-	if (!creds.success) {
-		return res.status(400).json({ message: "Error en los datos enviados" })
+		if (!creds.success) {
+			return res.status(400).json({ message: "Error en los datos enviados" })
+		}
+		try {
+			await prisma.contact.create({
+				data: {
+					fullName: req.body.fullName,
+					email: req.body.email,
+					subject: req.body.subject,
+					theme: req.body.theme,
+				},
+			})
+			return res.status(200).json({ message: "registrado correctamente" })
+		}
+		catch (err) {
+			return res.status(400).json({ message: err })
+		}
 	}
-	try {
-		await prisma.contact.create({
-			data: {
-				fullName: req.body.fullName,
-				email: req.body.email,
-				subject: req.body.subject,
-				theme: req.body.theme,
-			},
-		})
-		return res.status(200).json({ message: "registrado correctamente" })
-	}
-	catch (err) {
-		return res.status(400).json({ message: err })
-	}
-	}
-
 }
 
 export default handler
